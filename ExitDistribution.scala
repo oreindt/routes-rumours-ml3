24416d89ca631eb6991175c5a065cad483790bf3
Oliver Reinhardt <oliver.reinhardt@uni-rostock.de>
2019-05-10 19:22:42 +0200

    named things, commented, experiment as in the paper

--- ExitDistribution.scala
+++ ExitDistribution.scala
@@ -19,30 +19,24 @@ object ExitDistribution extends App {
       initializeWith(() => new JsonStateBuilder("init50.json"))
 
 
-      scan("p_find_links" <~ (0.1,0.5,1.0)
-        and "p_find_dests" <~ (0.1,0.3,1.0)
-        and "speed_expl_stay" <~ (0.3,0.5,1.0)
-        and "speed_expl_move" <~ (0.3,0.5,1.0),
-        "p_keep_contact" <~ (0.1,0.3,0.6)
-        and "p_info_mingle" <~ (0.1,0.3,0.6)
-        and "p_info_contacts" <~ (0.1,0.3,0.6)
-        and "p_transfer_info" <~ (0.1,0.3,0.6))
+    set("p_find_links" <~ 0.5)
+    set("p_find_dests" <~ 0.3)
+    set("speed_expl_stay" <~ 1.0)
+    set("speed_expl_move" <~ 1.0)
+    set("p_keep_contact" <~ 0.1)
+    set("p_info_mingle" <~ 0.1)
+    set("p_info_contacts" <~ 0.1)
+    set("p_transfer_info" <~ 0.1)
 
-
-      observeAt(stopTime)
-      //observe("fired" ~ expressionDistribution(agentType = "World", expression = "ego.fired"))
-      observe("migrants" ~ agentCount(agentType = "Migrant")) // this only counts agents who are alive, i.e., have not arrived at an exit
-      observe("info_loc" ~ agentCount(agentType = "Information"))
-      observe("info_link" ~ agentCount(agentType = "InfoLink"))
-      observe("cities" ~ agentCount(agentType = "Location"))
-      observe("links" ~ agentCount(agentType = "Link"))
-      //observe("exits" ~ agentCount(agentType = "Location", filter = "ego.type = 'exit'"))
-      //observe("entries" ~ agentCount(agentType = "Location", filter = "ego.type = 'entry'"))
-      //observe("cities" ~ agentCount(agentType = "Location", filter = "ego.type = 'std'"))
-      //observe("entering" ~ expressionDistribution(agentType = "Location", filter = "ego.type = 'entry'", expression = "ego.migrants.size()"))
-      observe("exiting" ~ expressionDistribution(agentType = "Location", filter = "ego.type = 'exit'", expression = "ego.migrants.size()"))
-      observe("y" ~ expressionDistribution(agentType = "Location", filter = "ego.type = 'exit'", expression = "ego.y"))
-      withRunResult(writeCSV)
+    observeAt(stopTime)
+    observe("migrants" ~ agentCount(agentType = "Migrant")) // this only counts agents who are alive, i.e., have not arrived at an exit
+    observe("info_loc" ~ agentCount(agentType = "Information"))
+    observe("info_link" ~ agentCount(agentType = "InfoLink"))
+    observe("cities" ~ agentCount(agentType = "Location"))
+    observe("links" ~ agentCount(agentType = "Link"))
+    observe("exiting" ~ expressionDistribution(agentType = "Location", filter = "ego.type = 'exit'", expression = "ego.migrants.size()"))
+    observe("y" ~ expressionDistribution(agentType = "Location", filter = "ego.type = 'exit'", expression = "ego.y"))
+    withRunResult(writeCSV)
     }
   execute(experiment)
   System.out.println(experiment.executionTime.get / (1000 * 60) + " min")
